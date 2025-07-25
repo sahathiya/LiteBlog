10e2df64ae7ba721f53a405414469efcc32d4f30
sahathiya <salamkaraya4@gmail.com>
2025-07-25 12:07:25 +0530

    set up the issue5

--- blog/src/app/blog/blogs/[id]/page.jsx
+++ blog/src/app/blog/blogs/[id]/page.jsx
@@ -5,29 +5,34 @@ import axios from 'axios';
 import React from 'react'
 
 
-export async function generateMetadata({ params }) {
-const baseurl=process.env.NEXT_PUBLIC_BASE_URL
-    const response = await axios.get(`${baseurl}/api/blogs/details/${params.id}`);
-    const blog = response.data;
-console.log("params",await params);
+// export async function generateMetadata({ params }) {
+// const baseurl=process.env.NEXT_PUBLIC_BASE_URL
+// console.log("baseurl",baseurl);
 
-    return {
-      title: blog.title,
-      description: blog.content?.slice(0, 150) || 'Blog post detail',
+//     const response = await axios.get(`${baseurl}/api/blogs/details/${params.id}`);
+//     const blog = response.data;
+// console.log("params",await params);
+
+//     return {
+//       title: blog.title,
+//       description: blog.content?.slice(0, 150) || 'Blog post detail',
       
-      openGraph:{
-       images:[
-        {
-          url:blog.image
-        }
-       ]
+//       openGraph:{
+//        images:[
+//         {
+//           url:blog.image
+//         }
+//        ]
     
-    }
+//     }
 
-}
-}
+// }
+// }
 function page({params}) {
      const id = params.id;
+    const baseurl=process.env.NEXT_PUBLIC_API_URL
+    console.log("baseurl...",baseurl);
+    
   return (
     <div>
       <BlogDetails id={id}/>
