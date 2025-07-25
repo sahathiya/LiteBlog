bf3da3da86cda75c5b8acac785af4f1f33633e9a
sahathiya <salamkaraya4@gmail.com>
2025-07-25 11:40:58 +0530

    set up the issue3

--- blog/src/app/blog/blogs/[id]/page.jsx
+++ blog/src/app/blog/blogs/[id]/page.jsx
@@ -5,27 +5,27 @@ import axios from 'axios';
 import React from 'react'
 
 
-export async function generateMetadata({ params }) {
-const baseurl=process.env.NEXT_PUBLIC_BASE_URL
-    const response = await axios.get(`${baseurl}/api/blogs/details/${params.id}`);
-    const blog = response.data;
-console.log("params",await params);
+// export async function generateMetadata({ params }) {
+// const baseurl=process.env.NEXT_PUBLIC_BASE_URL
+//     const response = await axios.get(`${baseurl}/api/blogs/details/${params.id}`);
+//     const blog = response.data;
+// console.log("params",await params);
 
-    return {
-      title: blog.title,
-      description: blog.content?.slice(0, 150) || 'Blog post detail',
+//     return {
+//       title: blog.title,
+//       description: blog.content?.slice(0, 150) || 'Blog post detail',
       
-      openGraph:{
-       images:[
-        {
-          url:blog.image
-        }
-       ]
+//       openGraph:{
+//        images:[
+//         {
+//           url:blog.image
+//         }
+//        ]
     
-    }
+//     }
 
-}
-}
+// }
+// }
 function page({params}) {
      const id = params.id;
   return (
