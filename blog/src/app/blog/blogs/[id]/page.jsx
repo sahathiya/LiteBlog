633e64d2b31de8c8d9db74e6285e1918cea77d40
sahathiya <salamkaraya4@gmail.com>
2025-07-25 12:10:49 +0530

    set up the seo in dynamic

--- blog/src/app/blog/blogs/[id]/page.jsx
+++ blog/src/app/blog/blogs/[id]/page.jsx
@@ -5,29 +5,29 @@ import axios from 'axios';
 import React from 'react'
 
 
-// export async function generateMetadata({ params }) {
-// const baseurl=process.env.NEXT_PUBLIC_BASE_URL
-// console.log("baseurl",baseurl);
+export async function generateMetadata({ params }) {
+const baseurl=process.env.NEXT_PUBLIC_BASE_URL
+console.log("baseurl",baseurl);
 
-//     const response = await axios.get(`${baseurl}/api/blogs/details/${params.id}`);
-//     const blog = response.data;
-// console.log("params",await params);
+    const response = await axios.get(`${baseurl}/api/blogs/details/${params.id}`);
+    const blog = response.data;
+console.log("params",await params);
 
-//     return {
-//       title: blog.title,
-//       description: blog.content?.slice(0, 150) || 'Blog post detail',
+    return {
+      title: blog.title,
+      description: blog.content?.slice(0, 150) || 'Blog post detail',
       
-//       openGraph:{
-//        images:[
-//         {
-//           url:blog.image
-//         }
-//        ]
+      openGraph:{
+       images:[
+        {
+          url:blog.image
+        }
+       ]
     
-//     }
+    }
 
-// }
-// }
+}
+}
 function page({params}) {
      const id = params.id;
     const baseurl=process.env.NEXT_PUBLIC_API_URL
