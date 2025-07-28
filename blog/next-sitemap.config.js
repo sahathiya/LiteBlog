86949d91eb85e8d44e98e16dc153f192c5c61c88
sahathiya <salamkaraya4@gmail.com>
2025-07-28 09:51:51 +0530

    setup next-sitemap

---
+++ blog/next-sitemap.config.js
@@ -0,0 +1,6 @@
+//next-sitemap.config.js
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_API_URL, // Your website's URL
+  generateRobotsTxt: true, // (Optional) Generates a robots.txt file
+  
+}
